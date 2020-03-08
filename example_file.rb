4484957183b94430141310f606e4132d846762ab
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-03-08 02:33:39 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,14 @@
+while false do
+  puts "I will never run"
+end
+
+def method(matrix,new_value)
+spice_rack = [
+  ["Mace", "Ginger", "Morojam"],
+  ["Paprika", "Fajita Mix", "Coriander"],
+  ["Parsley", "Sage", "Rosemary"]
+  ]
+  
+  spice_rack[1][100] = "Poodle Dinner"
+  
+end
